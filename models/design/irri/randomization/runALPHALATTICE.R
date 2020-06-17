ef4dd289e5a13b726c9fe2100b177293ca2302dc
Alaine Gulles <alaine.a.gulles@gmail.com>
2020-06-17 11:59:05 +0800

    modify runALPHALATTICE to include entryList and use entry_id in the randomization

--- models/design/irri/randomization/runALPHALATTICE.R
+++ models/design/irri/randomization/runALPHALATTICE.R
@@ -1,18 +1,23 @@
 # -------------------------------------------------------------------------------------
 # Name             : runALPHALATTICE
 # Description      : Generate randomization and layout for Alpha Lattice Design which 
-#                    can be run in the command line with arguments
+#                    can be run in the command line with arguments and accepts entry list
 # R Version        : 3.5.1 
+# Note             : with entryList as argument and uses entry_id in the randomization
 # -------------------------------------------------------------------------------------
 # Author           : Alaine A. Gulles 
 # Author Email     : a.gulles@irri.org
-# Date             : 2019.03.01
+# Date             : 2020.06.17
 # Maintainer       : Alaine A. Gulles 
 # Maintainer Email : a.gulles@irri.org
-# Script Version   : 1
+# Script Version   : 2
+# Command          : Rscript runALPHALATTICE.R --entryList "D:SampleEntryList1_n24.csv"
+#                    --nTrial 3 --nRep 4 --nBlk 4 --genLayout T --nRowPerBlk 2 
+#                    --nRowPerRep 4 --nFieldRow 8 --serpentine F -o "Alpha_Small_Output1" 
+#                    -p "D:/Results/AlphaLattice Result 2020-06-17/WithLayout"
 # -------------------------------------------------------------------------------------
 # Parameters:
-# nTreatment = number of entries
+# entryList = a cvs file containing the entry information
 # nRep = number of replicates
 # nBlk = number of blocks per replicate
 # nTrial = number of trials (location rep)
@@ -25,12 +30,13 @@
 # outputPath = path where output will be saved
 # ---------------------------------------------------------
 
+# load the needed packages
 suppressWarnings(suppressPackageStartupMessages(library(optparse)))
 suppressWarnings(suppressPackageStartupMessages(library(PBTools)))
 
 optionList <- list(
-  make_option(opt_str = c("-n","--nTreatment"), type = "integer", default = NULL,
-              help = "Number of entries", metavar = "number of entries"),
+  make_option(opt_str = c("--entryList"), type = "character", 
+              help = "Entry List", metavar = "entry list"),
   make_option(opt_str = c("-r","--nRep"), type = "integer", default = NULL,
               help = "Number of replicates", metavar = "number of replicates"),
   make_option(opt_str = c("-b","--nBlk"), type = "integer", default = NULL,
@@ -55,20 +61,26 @@ optionList <- list(
               metavar = "path where output will be saved")
 )
 
+# garbage collection 
+gc()
+
 # create an instance of a parser object
 opt_parser = OptionParser(option_list = optionList)
 opt = parse_args(opt_parser)
 
 # check if folder is exist or not
-
 if (!dir.exists(opt$outputPath)) {
   dir.create(opt$outputPath)
 }
 
+# read the file containing the entry list
+entryData <- read.csv(file = opt$entryList)
+entryInfo <- entryData[,"entry_id"]
+
 # write the design information
 sink(file = paste(paste(opt$outputPath, opt$outputFile, sep = "/"), "_designInfo.txt", sep = ""))
 if (opt$genLayout) {
-  temp <- try(result <- designAlphaLattice(generate = list(Entry = opt$nTreatment),
+  temp <- try(result <- designAlphaLattice(generate = list(Entry = entryInfo),
                                            numBlk = opt$nBlk,
                                            numRep = opt$nRep,
                                            numTrial = opt$nTrial,
@@ -80,7 +92,7 @@ if (opt$genLayout) {
                                            display = TRUE),
               silent = TRUE)
 } else {
-  temp <- try(result <- designAlphaLattice(generate = list(Entry = opt$nTreatment),
+  temp <- try(result <- designAlphaLattice(generate = list(Entry = entryInfo),
                                            numBlk = opt$nBlk,
                                            numRep = opt$nRep,
                                            numTrial = opt$nTrial,
@@ -99,28 +111,12 @@ sink()
 
 if(all(class(temp) == "try-error")) { stop(paste("Error in designAlphaLattice:", msg, sep = "")) }
 
+# rename columns
+fbook <- result[[1]]
+names(fbook) <- c("occurrence", "replicate", "block", "entry_id","plot_number", "field_row", "field_col")
+
+# rearrange columns
+nfbook <- fbook[,c("occurrence", "plot_number", "replicate", "block", "entry_id","field_row", "field_col")]
+
 # save the fieldbook to a csv file
 write.csv(result$fieldbook, file = paste(paste(opt$outputPath, opt$outputFile, sep = "/"), "_fieldbook.csv", sep = ""), row.names = FALSE)
-
-if (opt$genLayout) {
-  for (i in (1:length(result$plan$TrmtLayout))) {
-    temp <- result$plan$TrmtLayout[[i]]
-    write.csv(temp, file = paste(paste(opt$outputPath, opt$outputFile, sep = "/"), "_TrmtLayout_",names(result$plan$TrmtLayout)[i], ".csv", sep = ""))
-  }
-  
-  for (i in 2:length(result$plan)) {
-    write.csv(result$plan[[i]], file = paste(paste(opt$outputPath, opt$outputFile, sep = "/"), "_", names(result$plan)[i],".csv", sep = ""))
-  }
-} else {
-  for (i in (1:length(unique(result$plan$TrmtLayout[,1])))) {
-    tempTL <- result$plan$TrmtLayout[result$plan$TrmtLayout[,1] == unique(result$plan$TrmtLayout[,1])[i],]
-    write.csv(tempTL, file = paste(paste(opt$outputPath, opt$outputFile, sep = "/"), "_StatisticalDesignArray_", names(result$plan$TrmtLayout)[1], i, ".csv", sep = ""), row.names = FALSE)
-    if (i == 1) {
-      tempBL <- result$plan$BlockLayout[result$plan$BlockLayout[,1] == unique(result$plan$BlockLayout[,1])[i],]
-      tempBL <- tempBL[,2:ncol(tempBL)]
-      write.csv(tempBL, file = paste(paste(opt$outputPath, opt$outputFile, sep = "/"), "_", names(result$plan)[2],".csv", sep = ""), row.names = FALSE)
-    }
-  }
-  
-}
-
