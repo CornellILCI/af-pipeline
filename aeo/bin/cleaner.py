e762745b081e0388408f1f2e9969b54f17b59cfb
Victor Jun Ulat <vjmulat@gmail.com>
2020-07-08 16:25:45 -0500

    Added functionality to clean output folder.

--- aeo/bin/cleaner.py
+++ aeo/bin/cleaner.py
@@ -20,21 +20,47 @@ import simbaUtils
 
 simbaUtils.readConfig()
 logPath=simbaUtils.cfg['lgd']
+outPath=simbaUtils.cfg['out']
 
-print(logPath)
+# print(logPath)
+# print(outPath)
 
+n=1
 files=[]
+folders=[]
+monthAgo=datetime.now() - timedelta(days=1)
 
+# delete slurm log files that are a month old
 for r, d, f in os.walk(logPath):
   for file in f:
+    # Ignore simba.LOG and README
     if not '.LOG' in file and not 'README' in file:
       files.append(os.path.join(r,file))
 
 for f in files:
-  fsize=os.stat(f).st_size
-  monthAgo=datetime.now() - timedelta(days=30)
   ftime=datetime.fromtimestamp(os.stat(f).st_mtime)
   
+  # print (f, "\t", ftime, "\t", monthAgo)
+
   if ftime < monthAgo:
     # delete files older than a month
-    print(f, fsize, monthAgo)
+    cmd="rm -fr " + f
+    os.system(cmd)
+    print(f, "\t", ftime)
+
+# delete folders that are a month old.
+for r, d, f in os.walk(outPath):
+  for dir in d:
+    if '_SD_0000' in dir:
+      folders.append(os.path.join(r,dir))
+
+for d in folders:
+  dtime=datetime.fromtimestamp(os.stat(d).st_mtime)
+ 
+  # print(d,"\t", dtime)
+
+  if dtime < monthAgo:
+    # delete folders older than a month
+    cmd="rm -fr " + d
+    os.system(cmd)
+    print(d, "\t", dtime)
