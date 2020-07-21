23fe810e83d7f290eb09c7f7c175e087ed3cd1e1
Victor Jun Ulat <vjmulat@gmail.com>
2020-07-21 17:54:21 -0500

    Modified schema.

--- aeo/database/2020.07.21-analytics.pg.sql
+++ aeo/database/2020.07.21-analytics.pg.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 10.6 (Ubuntu 10.6-0ubuntu0.18.04.1)
--- Dumped by pg_dump version 10.6 (Ubuntu 10.6-0ubuntu0.18.04.1)
+-- Dumped from database version 10.12 (Ubuntu 10.12-0ubuntu0.18.04.1)
+-- Dumped by pg_dump version 10.12 (Ubuntu 10.12-0ubuntu0.18.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -12,6 +12,7 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
@@ -42,7 +43,9 @@ CREATE TABLE public.analysis (
     request_id character varying(255),
     sha character varying(255),
     request_type character varying(225),
-    time_submitted timestamp without time zone
+    time_submitted timestamp without time zone,
+    status character varying,
+    msg text
 );
 
 
