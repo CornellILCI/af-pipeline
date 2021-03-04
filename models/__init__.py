14ab5c8b0f74282897ff6d80407448c46184028f
vg249 <vg249@cornell.edu>
2021-03-04 17:56:27 -0500

    import class objects from module

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,13 @@
 from .experiment import Experiment
 from .api_response import ApiResponse
+from .occurrence import OccurrenceEbs
+from .occurrence import Occurrence
+from .trait import Trait
 
 __all__ = [
     "Experiment",
-    "ApiResponse"
+    "ApiResponse",
+    "Occurrence",
+    "OccurrenceEbs",
+    "Trait"
 ]
