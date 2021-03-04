13606230c66b30c22f06f7fc77134d19ad53adb1
vg249 <vg249@cornell.edu>
2021-03-04 17:53:30 -0500

    added mock test data method to conftest

--- conftest.py
+++ conftest.py
@@ -3,22 +3,29 @@ import os
 
 import pandas as pd
 
+from models import Occurrence
+
 import json
 
 os.environ["B4R_API_BASE_URL"] = ""
 
+def __read_mock_json_file(file_path):
+    mock_response_file_path = os.path.join(sys.path[0], file_path)
+    with open(mock_response_file_path) as mock_response_file:
+        test_response = json.load(mock_response_file)
+    return test_response
 
 def get_ebs_plots_response():
     """ returns a plots response json object to be used as mock """
 
-    mock_response_file_path = (
-        os.path.join(sys.path[0],
-                     "tests/data_reader/plots_mock_response.json"))
-    with open(mock_response_file_path) as mock_response_file:
-        test_plot_data = json.load(mock_response_file)
+    return __read_mock_json_file("tests/data_reader/plots_mock_response.json")
 
-    return test_plot_data
 
+def get_ebs_occurrence_response():
+    """ returns a occurrence response json object to be used as mock """
+
+    return __read_mock_json_file(
+        "tests/data_reader/occurrence_mock_response.json")
 
 def get_ebs_unauthorized_error_response():
     error_response = """{
@@ -44,15 +51,28 @@ def get_ebs_unauthorized_error_response():
     return json.loads(error_response)
 
 
-def get_test_plots():
+def get_test_plots() -> pd.DataFrame:
     """ return a mock plots dataframe """
+    columns = ["plot_id", "experiment_id", "location_id", "occurrence_id",
+               "entry_id", "pa_x", "pa_y", "rep_factor",
+               "blk", "plot_qc"]
+    data = [
+        [2909, 4, 6, 7, 180, 3, 5, 1, 1, "G"],
+        [2910, 4, 6, 7, 103, 4, 5, 1, 1, "G"],
+    ]
+    return pd.DataFrame(data, columns=columns)
 
-    from data_reader.phenotype_data_ebs import PhenotypeDataEbs
 
-    test_plot_data = get_ebs_plots_response()
-    plots_test_df = pd.DataFrame(test_plot_data["result"]["data"])
-    plots_test_df.rename(
-        columns=PhenotypeDataEbs.API_FIELDS_TO_LOCAL_FIELDS,
-        inplace=True
-    )
-    return plots_test_df
+def get_test_occurrence() -> Occurrence:
+    test_occurrence = {
+        "occurrence_id": 7,
+        "occurrence_name": "test_occurrence",
+        "experiment_id": 4,
+        "experiment_name": "test_experiment",
+        "location_id": 6,
+        "location": "test_location",
+        "rep_count": 1,
+        "entry_count": 4,
+        "plot_count": 2
+    }
+    return Occurrence(**test_occurrence)
