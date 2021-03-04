63498aa13ba6316cbe17502d891e6884b78ae785
vg249 <vg249@cornell.edu>
2021-03-04 01:14:42 -0500

    moved untit test method as TestCase class method to use inbuild unittest functionality

--- tests/data_reader/test_phenotype_data_ebs.py
+++ tests/data_reader/test_phenotype_data_ebs.py
@@ -1,23 +1,50 @@
+from unittest import TestCase
 from unittest.mock import patch
+
 import pandas as pd
 from pandas._testing import assert_frame_equal
 
-from conftest import get_ebs_plots_response, get_test_plots
+from conftest import (
+    get_ebs_plots_response,
+    get_test_plots,
+    get_ebs_unauthorized_error_response)
+
+from exceptions import DataReaderException
 
 
-@patch('data_reader.phenotype_data_ebs.requests.get')
-def test_get_plots_by_occurrence_id(mock_get_request):
+class TestPhenotypeDataEbs(TestCase):
 
-    from data_reader.phenotype_data_ebs import PhenotypeDataEbs
+    @patch('data_reader.data_reader.requests.post')
+    def test_get_plots_by_occurrence_id(self, mock_post):
 
-    mock_get_request.return_value.ok = True
-    mock_get_request.return_value.json.return_value = get_ebs_plots_response()
+        from data_reader.phenotype_data_ebs import PhenotypeDataEbs
 
-    plots_test_df = get_test_plots()
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json.return_value = (
+            get_ebs_plots_response())
 
-    plots_result_df = PhenotypeDataEbs().get_plots_by_occurrence_id(4)
+        plots_test_df = get_test_plots()
 
-    # assert dataframe is returned
-    assert isinstance(plots_result_df, pd.DataFrame)
+        plots_result_df = PhenotypeDataEbs(
+            api_base_url="http://test").get_plots_by_occurrence_id(4)
 
-    assert_frame_equal(plots_result_df, plots_test_df)
+        # assert dataframe is returned
+        assert isinstance(plots_result_df, pd.DataFrame)
+
+        assert_frame_equal(plots_result_df, plots_test_df)
+
+        mock_post.return_value.status_code = 401
+        mock_post.return_value.json.return_value = (
+            get_ebs_unauthorized_error_response)
+
+        with self.assertRaises(DataReaderException):
+            plots_result_df = PhenotypeDataEbs(
+                api_base_url="http://test").get_plots_by_occurrence_id(4)
+
+        with self.assertRaises(DataReaderException):
+            plots_result_df = PhenotypeDataEbs(
+                api_base_url="htp://test").get_plots_by_occurrence_id(4)
+
+    @patch('data_reader.data_reader.requests.get')
+    def test_get_occurrence(self, mock_get):
+        pass
