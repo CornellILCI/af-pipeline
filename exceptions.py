2cce06eef73c9a45ebff1f29c83d080644582ae4
vg249 <vg249@cornell.edu>
2021-03-04 01:13:10 -0500

    removed ApiConnectionError and defined it as DataReaderException

--- exceptions.py
+++ exceptions.py
@@ -1,12 +1,8 @@
 
 
 class DataReaderException(Exception):
-    pass
-
-
-class ApiConnectionError(DataReaderException):
-    def __init__(self, api_url):
-        self.api_url = api_url
+    def __init__(self, message):
+        self.message = message
 
     def __str__(self):
-        return repr("Unable to connect to the API {0}".format(self.api_url))
+        return repr(self.message)
