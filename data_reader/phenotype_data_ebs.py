6f8855af75e0bd5103b1de2fcfb6c42604f266c8
vg249 <vg249@cornell.edu>
2021-03-04 17:55:52 -0500

    finished get plots implementation and added get occurrence implementation

--- data_reader/phenotype_data_ebs.py
+++ data_reader/phenotype_data_ebs.py
@@ -1,20 +1,23 @@
 import pandas as pd
 
-from models.experiment import Experiment
-from models.trait import Trait
+from pydantic import ValidationError
+
+from models import Experiment, Occurrence, Trait, OccurrenceEbs
 
 from data_reader.phenotype_data import PhenotypeData
 
 from exceptions import DataReaderException
 
-SEARCH_PLOTS_BY_OCCURRENCE_URL = "/occurrences/{occurrence_id}/plots-search"
+SEARCH_PLOTS_ENDPOINT = "/plots-search"
+
+SEARCH_OCCRRENCES_ENDPOINT = "/occurrences-search"
 
 
 class PhenotypeDataEbs(PhenotypeData):
     """ reads phenotype data from a ebs data source.
     """
 
-    API_FIELDS_TO_LOCAL_FIELDS = {
+    PLOTS_API_FIELDS_TO_LOCAL_FIELDS = {
         "plotDbId": "plot_id",
         "entryDbId": "entry_id",
         "paX": "pa_x",
@@ -24,13 +27,14 @@ class PhenotypeDataEbs(PhenotypeData):
         "plotQcCode": "plot_qc"
     }
 
-    def get_plots_by_occurrence_id(self,
-                                   occurrence_id: int = None) -> pd.DataFrame:
+    def get_plots(self, occurrence_id: str = None) -> pd.DataFrame:
 
-        plots_endpoint = SEARCH_PLOTS_BY_OCCURRENCE_URL.format(
-            occurrence_id=occurrence_id)
+        search_query = {
+            "occurrenceDbId": occurrence_id
+        }
 
-        api_response = self.post(endpoint=plots_endpoint)
+        api_response = self.post(endpoint=SEARCH_PLOTS_ENDPOINT,
+                                 data=search_query)
 
         if not api_response.is_success:
             raise DataReaderException(api_response.error)
@@ -38,20 +42,77 @@ class PhenotypeDataEbs(PhenotypeData):
         plots_data = api_response.body["result"]["data"]
 
         plots_df = pd.DataFrame(plots_data)
+
+        # keep only local field columns
+        plots_df_columns_to_drop = (
+            set(plots_df.columns) -
+            self.PLOTS_API_FIELDS_TO_LOCAL_FIELDS.keys())
+        plots_df_columns_to_keep = (
+            self.PLOTS_API_FIELDS_TO_LOCAL_FIELDS.keys() -
+            plots_df_columns_to_drop)
+        plots_df = plots_df[plots_df_columns_to_keep]
+
+        # rename dataframe column with local field names
         plots_df.rename(
-            columns=self.API_FIELDS_TO_LOCAL_FIELDS,
+            columns=self.PLOTS_API_FIELDS_TO_LOCAL_FIELDS,
             inplace=True
         )
 
+        occurrence = self.get_occurrence(occurrence_id)
+
+        columns_from_occurrence = {"experiment_id",
+                                   "occurrence_id",
+                                   "location_id"}
+
+        for column in columns_from_occurrence:
+            plots_df[column] = occurrence.dict()[column]
+
         return plots_df
 
-    def get_plots_measurements_by_occurrence_id(
-            self,
-            occurrence_id: int = None) -> pd.DataFrame:
+    def get_plot_measurements(self, occurrence_id: str = None) -> pd.DataFrame:
         raise NotImplementedError
 
-    def get_experiment(self, experiment_id: int = None) -> Experiment:
+    def get_occurrence(self, occurrence_id: str = None) -> Occurrence:
+
+        search_query = {
+            "occurrenceDbId": occurrence_id
+        }
+
+        api_response = self.post(endpoint=SEARCH_OCCRRENCES_ENDPOINT,
+                                 data=search_query)
+
+        if not api_response.is_success:
+            raise DataReaderException(api_response.error)
+
+        result_list = api_response.body["result"]["data"]
+
+        if len(result_list) > 1:
+            raise DataReaderException("More than one resource found for id")
+        elif len(result_list) == 0:
+            raise DataReaderException("No Occurrence found")
+
+        # load it to model to make sure required fields are found
+        try:
+            _occurrence_ebs = OccurrenceEbs(**result_list[0])
+        except ValidationError as e:
+            raise DataReaderException(str(e))
+
+        occurrence = {
+            "occurrence_id": _occurrence_ebs.occurrenceDbId,
+            "occurrence_name": _occurrence_ebs.occurrenceName,
+            "experiment_id": _occurrence_ebs.experimentDbId,
+            "experiment_name": _occurrence_ebs.experiment,
+            "location_id": _occurrence_ebs.locationDbId,
+            "location": _occurrence_ebs.location,
+            "rep_count": _occurrence_ebs.repCount,
+            "entry_count": _occurrence_ebs.entryCount,
+            "plot_count": _occurrence_ebs.plotCount
+        }
+
+        return Occurrence(**occurrence)
+
+    def get_experiment(self, experiment_id: str = None) -> Experiment:
         raise NotImplementedError
 
-    def get_trait(self, trait_id: int = None) -> Trait:
+    def get_trait(self, trait_id: str = None) -> Trait:
         raise NotImplementedError
