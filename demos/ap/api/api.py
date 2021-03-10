8c6cf53880382b6024cff32192fbaa754b859748
Rodolfo N. Duldulao, Jr <rnduldulaojr@gmail.com>
2021-03-11 05:59:23 +0800

    Reverted to pika for AMQP handling

--- demos/ap/api/api.py
+++ demos/ap/api/api.py
@@ -1,18 +1,15 @@
 import os
-import ssl
 
 from flask import Flask, request, jsonify
-from rabbitmq_pika_flask import RabbitMQ
 from pika import BlockingConnection, ConnectionParameters
-from pika.connection import SSLOptions
 from pika.credentials import PlainCredentials
 
 app = Flask(__name__)
 
-rabbit = RabbitMQ(app, use_ssl=False)
 
-#monkey patch
-rabbit.getConnection = lambda: BlockingConnection(ConnectionParameters(
+def get_connection():
+    return (
+        BlockingConnection(ConnectionParameters(
             host=os.getenv('MQ_HOST'),
             port=os.getenv('MQ_PORT'),
             credentials=PlainCredentials(
@@ -20,19 +17,26 @@ rabbit.getConnection = lambda: BlockingConnection(ConnectionParameters(
                 password=os.getenv('MQ_PASS')
             )
         ))
+    )
+
 
 CONSUMER_QUEUE = os.getenv("CONSUMER_QUEUE")
-
 
 @app.route('/jobs', methods=['POST'])
 def create_job():
     content = request.json
-    rabbit.send(body=jsonify(content), routing_key=CONSUMER_QUEUE)
+    print(jsonify(content))
+    #
+    conn = get_connection()
+    channel = conn.channel()
+    channel.basic_publish(
+        exchange='',
+        routing_key=CONSUMER_QUEUE,
+        body=jsonify(content)
+    )
+    conn.close()
     return jsonify({"status": "ok"}), 201
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
-
-
