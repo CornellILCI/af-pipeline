5a0d03d74b6e604ab031ef4c315c932bdee501c0
vg249 <vg249@cornell.edu>
2021-03-22 15:49:22 -0400

    Move test specific functions from conftest to their respective test module
    Add test_get_plots_pages @test_phnotype_data_brapi to test brapi extraction of multi page response

--- conftest.py
+++ conftest.py
@@ -1,126 +1,13 @@
 import sys
 import os
 
-import pandas as pd
-
-from models import Occurrence
-
 import json
 
 os.environ["B4R_API_BASE_URL"] = ""
 
 
-def __read_mock_json_file(file_path):
+def read_mock_json_file(file_path):
     mock_response_file_path = os.path.join(sys.path[0], file_path)
     with open(mock_response_file_path) as mock_response_file:
         test_response = json.load(mock_response_file)
     return test_response
-
-
-def get_ebs_plots_response():
-    """ returns a plots response json object to be used as mock """
-
-    return __read_mock_json_file("tests/data_reader/plots_mock_response.json")
-
-
-def get_ebs_occurrence_response():
-    """ returns a occurrence response json object to be used as mock """
-
-    return __read_mock_json_file(
-        "tests/data_reader/occurrence_mock_response.json")
-
-
-def get_brapi_observation_units_response():
-    """ returns a mock brapi response for observation units """
-
-    return __read_mock_json_file(
-        "tests/data_reader/brapi_observationunits_mock_response.json")
-
-
-def get_brapi_observations_response():
-    """ returns a mock brapi response for observation units """
-
-    return __read_mock_json_file(
-        "tests/data_reader/brapi_observations_mock_response.json")
-
-
-def get_brapi_studies_response():
-    """ returns a mock brapi response for studies """
-
-    return __read_mock_json_file(
-        "tests/data_reader/brapi_studies_mock_response.json")
-
-
-def get_ebs_unauthorized_error_response():
-    error_response = """{
-        "metadata": {
-            "pagination": {
-                "pageSize": null,
-                "totalCount": null,
-                "currentPage": null,
-                "totalPages": null
-            },
-            "status": [
-                {
-                    "message": "",
-                    "messageType": "Unauthorized"
-                }
-            ],
-            "datafiles": []
-        },
-        "result": {
-            "data": []
-        }
-    }"""
-    return json.loads(error_response)
-
-
-def get_test_plots() -> pd.DataFrame:
-    """ return a mock plots dataframe """
-
-    columns = ["plot_id", "experiment_id", "location_id", "occurrence_id",
-               "entry_id", "pa_x", "pa_y", "rep_factor",
-               "blk", "plot_qc"]
-    data = [
-        [2909, 4, 6, 7, 180, 3, 5, 1, 1, "G"],
-        [2910, 4, 6, 7, 103, 4, 5, 1, 1, "G"],
-    ]
-    return pd.DataFrame(data, columns=columns)
-
-
-def get_test_plot_measurements() -> pd.DataFrame:
-    """ return a mock plot measurement dataframe"""
-
-    columns = ["plot_id", "trait_id", "trait_qc", "trait_value"]
-    data = [
-        [2909, 1, "G", 6.155850575],
-        [2910, 1, "G", 6.751358238],
-    ]
-    return pd.DataFrame(data, columns=columns)
-
-
-def get_test_occurrence() -> Occurrence:
-    test_occurrence = {
-        "occurrence_id": 7,
-        "occurrence_name": "test_occurrence",
-        "experiment_id": 4,
-        "experiment_name": "test_experiment",
-        "location_id": 6,
-        "location": "test_location",
-        "rep_count": 1,
-        "entry_count": 4,
-        "plot_count": 2
-    }
-    return Occurrence(**test_occurrence)
-
-
-def get_test_occurrence_brapi() -> Occurrence:
-    test_occurrence = {
-        "occurrence_id": 7,
-        "occurrence_name": "test_occurrence",
-        "experiment_id": 4,
-        "experiment_name": "test_experiment",
-        "location_id": 6,
-        "location": "test_location",
-    }
-    return Occurrence(**test_occurrence)
--- tests/data_reader/test_phenotype_data_brapi.py
+++ tests/data_reader/test_phenotype_data_brapi.py
@@ -5,15 +5,73 @@ import pandas as pd
 from pandas._testing import assert_frame_equal
 
 from conftest import (
-    get_brapi_observation_units_response,
-    get_brapi_observations_response,
-    get_brapi_studies_response,
-    get_test_plots,
-    get_test_plot_measurements,
-    get_test_occurrence_brapi,
+    read_mock_json_file
 )
 
+from models import Occurrence
+
 from data_reader.phenotype_data_brapi import PhenotypeDataBrapi
+
+import json
+
+
+def get_brapi_observation_units_response(page_size=None,
+                                         total_pages=1,
+                                         ):
+    """ returns a mock brapi response for observation units.
+    """
+    return read_mock_json_file(
+        "tests/data_reader/brapi_observationunits_mock_response.json")
+
+
+def get_brapi_observations_response():
+    """ returns a mock brapi response for observation units """
+
+    return read_mock_json_file(
+        "tests/data_reader/brapi_observations_mock_response.json")
+
+
+def get_brapi_studies_response():
+    """ returns a mock brapi response for studies """
+
+    return read_mock_json_file(
+        "tests/data_reader/brapi_studies_mock_response.json")
+
+
+def get_test_plots() -> pd.DataFrame:
+    """ return a mock plots dataframe """
+
+    columns = ["plot_id", "experiment_id", "location_id", "occurrence_id",
+               "entry_id", "pa_x", "pa_y", "rep_factor",
+               "blk", "plot_qc"]
+    data = [
+        [2909, 4, 6, 7, 180, 3, 5, 1, 1, "G"],
+        [2910, 4, 6, 7, 103, 4, 5, 1, 1, "G"],
+    ]
+    return pd.DataFrame(data, columns=columns)
+
+
+def get_test_plot_measurements() -> pd.DataFrame:
+    """ return a mock plot measurement dataframe"""
+
+    columns = ["plot_id", "trait_id", "trait_qc", "trait_value"]
+    data = [
+        [2909, 1, "G", 6.155850575],
+        [2910, 1, "G", 6.751358238],
+    ]
+    return pd.DataFrame(data, columns=columns)
+
+
+def get_test_occurrence_brapi() -> Occurrence:
+    test_occurrence = {
+        "occurrence_id": 7,
+        "occurrence_name": "test_occurrence",
+        "experiment_id": 4,
+        "experiment_name": "test_experiment",
+        "location_id": 6,
+        "location": "test_location",
+    }
+    return Occurrence(**test_occurrence)
 
 
 class TestPhenotypeDataBrapi(TestCase):
@@ -27,6 +85,48 @@ class TestPhenotypeDataBrapi(TestCase):
             get_brapi_observation_units_response()])
 
         plots_test_df = get_test_plots()
+
+        plots_result_df = PhenotypeDataBrapi(
+            api_base_url="http://test").get_plots("testid")
+
+        # assert dataframe is returned
+        assert isinstance(plots_result_df, pd.DataFrame)
+
+        # arrange columns
+        plots_result_df = plots_result_df[plots_test_df.columns]
+
+        assert_frame_equal(plots_result_df,
+                           plots_test_df.astype(str))
+
+    @patch('data_reader.data_reader.requests.get')
+    def test_get_plots_with_pages(self, mock_get):
+
+        mock_get.return_value.status_code = 200
+
+        pagination__ = """{
+            "pageSize": 2,
+            "totalPages": 2,
+            "currentPage": 0,
+            "totalCount": 3
+        }"""
+
+        first_page = get_brapi_observation_units_response()
+
+        pagination = json.loads(pagination__)
+        first_page["metadata"]["pagination"] = pagination
+
+        second_page = get_brapi_observation_units_response()
+        pagination = json.loads(pagination__)
+        pagination["currentPage"] = 1
+        second_page["metadata"]["pagination"] = pagination
+        second_page["result"]["data"].pop()
+
+        mock_get.return_value.json = Mock(
+            side_effect=[first_page, second_page])
+
+        plots_test_df = get_test_plots()
+
+        plots_test_df = plots_test_df.append(get_test_plots().iloc[0])
 
         plots_result_df = PhenotypeDataBrapi(
             api_base_url="http://test").get_plots("testid")
