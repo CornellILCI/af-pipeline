18adc15d99d00d13a6ba33b95e71a7c595285c96
sb2597 <62260853+sb2597@users.noreply.github.com>
2021-04-13 11:57:02 -0400

    Added API

--- af-jobs-api/api.py
+++ af-jobs-api/api.py
@@ -1,10 +1,10 @@
 import os
 import json
+import uuid
 
 from flask import Flask, request, jsonify, render_template
 from pika import BlockingConnection, ConnectionParameters
 from pika.credentials import PlainCredentials
-import uuid
 
 app = Flask(__name__)
 
@@ -12,36 +12,117 @@ app = Flask(__name__)
 def get_connection():
     return (
         BlockingConnection(ConnectionParameters(
-            host=os.getenv('MQ_HOST'),
-            port=os.getenv('MQ_PORT'),
+            host=os.getenv('MQ_HOST','localhost'),
+            port=os.getenv('MQ_PORT','5672'),
             credentials=PlainCredentials(
-                username=os.getenv('MQ_USER'),
-                password=os.getenv('MQ_PASS')
+                username=os.getenv('MQ_USER','admin'),
+                password=os.getenv('MQ_PASS','mypass')
             )
+            # host='localhost',
+            # port='5672',
+            # credentials=PlainCredentials(
+            #     username='admin',
+            #     password='mypass'
+            # )
         ))
     )
 
 
-CONSUMER_QUEUE = os.getenv("CONSUMER_QUEUE")
+CONSUMER_QUEUE = os.getenv("CONSUMER_QUEUE",'jobs')
 
-@app.route('/jobs', methods=['POST'])
-def create_job():
+#Example API endpoint
+# @app.route('/jobs', methods=['POST'])
+# def create_job():
+#     content = request.json
+
+#     #
+#     conn = get_connection()
+#     channel = conn.channel()
+#     channel.basic_publish(
+#         exchange='',
+#         body=json.dumps(content)
+#     )
+#     conn.close()
+#     return jsonify({"status": "ok"}), 201
+
+@app.route('/StartProcess', methods=['POST'])
+def start_process():
+    errorMessage = ''
     content = request.json
-    # assign job Id
-    job_id = uuid.uuid4()
-    content["jobId"] = job_id
-    
-    #
-    conn = get_connection()
-    channel = conn.channel()
-    channel.basic_publish(
-        exchange='',
-        routing_key=CONSUMER_QUEUE,
-        body=json.dumps(content)
-    )
-    conn.close()
-    return jsonify({"status": "ok", "jobId": job_id}), 201
+    processid = ''
 
+    if content == None: 
+        errorMessage = "Empty request. "
+    else:
+        # if "experimentId" not in content:
+        #     errorMessage += "experimentId does not exist in the request. "
+        if "dataSource" not in content:
+            errorMessage += "dataSource does not exist in the request. "
+        elif content["dataSource"] != "EBS" and content["dataSource"] != "BRAPI"
+            errorMessage += "dataSource is not 'EBS' or 'BRAPI'. "
+        if "dataSourceId" not in content:
+            errorMessage += "dataSourceId does not exist in the request. "
+
+        if "dataType" not in content:
+            errorMessage += "dataType does not exist in the request. "
+        elif content["dataType"] != "GENOTYPE" and content["dataType"] != "PHENOTYPE"
+            errorMessage += "dataSource is not 'GENOTYPE' or 'PHENOTYPE'. "
+
+        if "token" not in content:
+            errorMessage += "token does not exist in the request. "
+        if "processName" not in content:
+            errorMessage += "processName does not exist in the request. "    
+
+# experimentId - optional
+# dataSource - required EBS or BRAPI
+# dataSourceId - example EBS1 EBS2 BRAPI1
+# dataType - PHENOTYPE or GENOTYPE
+# occurrenceId - optional
+# traitId - optional
+# token - 
+# processName - required
+
+
+
+# returns
+# jobId
+
+
+    # TODO we will need further validations on the request
+
+    if(errorMessage == ''):  
+        processid = str(uuid.uuid4())  
+        message = {
+            "dataSource" : content["dataSource"],
+            "dataSourceId" : content["dataSourceId"],
+            "dataType" : content["dataType"],
+            "processName" : content["processName"],
+            "token" : content["token"],
+            "processId" : processid
+        }
+        # occurrenceId - optional
+        if "occurrenceId" not in content:
+            message['occurrenceId'] = content['occurrenceId']
+        # traitId - optional
+        if "traitId" not in content:
+            message['traitId'] = content['traitId']
+        # experimentId - optional
+        if "experimentId" not in content:
+            message['experimentId'] = content['experimentId']
+
+        conn = get_connection()
+        channel = conn.channel()
+        channel.basic_publish(
+            exchange='',
+            routing_key=CONSUMER_QUEUE,
+            body=json.dumps(message)
+        )
+        conn.close()
+
+    if(errorMessage != ''):
+        return jsonify({"status": "error", "message": errorMessage}), 201
+    else:
+        return jsonify({"status": "ok", "Process ID" : processid}), 201
 
 @app.route('/test', methods=['GET'])
 def test():
