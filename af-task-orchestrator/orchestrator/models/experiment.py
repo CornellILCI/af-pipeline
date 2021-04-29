d6dcd9635c5dfe67619c75a3910ee80d60eb318b
vg249 <vg249@cornell.edu>
2021-04-29 07:41:42 -0400

    Setup toplevel pipeline module that can be run by both CELERY and SLURM executors

--- af-task-orchestrator/orchestrator/models/experiment.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class Experiment(BaseModel):
-    id: int
-    experiment_name: str
--- af-task-orchestrator/orchestrator/models/trait.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class Trait(BaseModel):
-    id: int
-    trait_name: str
