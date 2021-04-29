f587bd4d35d18b12c9e5807076554a967bd7ead2
vg249 <vg249@cornell.edu>
2021-04-29 09:18:39 -0400

    Setup toplevel pipeline module that can be run by both CELERY and SLURM executors

---
+++ pipeline/data_gather/data_gather.py
@@ -0,0 +1,10 @@
+from .data_reader import DataReaderFactory
+
+
+class DataProcess:
+
+    def __init__(self, some=None):
+        pass
+
+    def run(self):
+        pass
---
+++ pipeline/data_process_pipeline_script.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from data_gather.data_gather import DataProcess
+
+if __name__ == '__main__':
+    DataProcess("").run()
