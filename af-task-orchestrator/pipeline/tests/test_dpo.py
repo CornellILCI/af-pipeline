9384d4b5cb43feb8c79a5b40cce951de5cc80b8e
vg249 <vg249@cornell.edu>
2021-05-10 08:53:06 -0400

    Add a test case for seml filter in DPO

--- af-task-orchestrator/pipeline/tests/test_dpo.py
+++ af-task-orchestrator/pipeline/tests/test_dpo.py
@@ -145,12 +145,136 @@ class TestProcessData(TestCase):
         self.assertTrue("data_file" in results[0])
         with open(results[0]["data_file"]) as data_f_:
             data_file_contents = data_f_.read()
-            print(data_file_contents)
-            print(expected_data_file_contents)
             self.assertEqual(data_file_contents, expected_data_file_contents)
 
     @patch("pipeline.data_reader.phenotype_data_ebs.PhenotypeDataEbs.get_trait")
     @patch("pipeline.data_reader.phenotype_data_ebs.PhenotypeDataEbs.get_plot_measurements")
     @patch("pipeline.data_reader.phenotype_data_ebs.PhenotypeDataEbs.get_plots")
     def test_dpo_seml_filter(self, mock_get_plots, mock_get_plot_measurements, mock_get_trait):
-        pass
+        test_request = get_json_resource(__file__, "test_analysis_request.req")
+        test_config = get_json_resource(__file__, "test_analysis_config.cfg")
+
+        test_request["metadata"]["id"] = "test_id"
+
+        # set analysis pattern to 1 so sesl filter will be called.
+        test_request["parameters"]["exptloc_analysis_pattern"] = 2
+
+        test_request["data"]["occurrence_id"] = [1, 2]
+        test_request["data"]["trait_id"] = [1]
+
+        mock_plots = []
+        plots_columns = [
+            "plot_id",
+            "expt_id",
+            "loc_id",
+            "occurr_id",
+            "entry_id",
+            "pa_x",
+            "pa_y",
+            "rep_factor",
+            "blk",
+            "plot_qc",
+        ]
+
+        # for occurrence id 1
+        mock_plots.append(
+            DataFrame(
+                columns=plots_columns,
+                data=[
+                    [2909, 1, 1, 1, 1, 1, 1, 1, 1, "G"],
+                    [2910, 1, 1, 1, 1, 1, 2, 1, 1, "G"],
+                ],
+            )
+        )
+
+        # for occurrence id 2
+        mock_plots.append(
+            DataFrame(
+                columns=plots_columns,
+                data=[
+                    [2911, 1, 1, 2, 1, 2, 1, 1, 1, "G"],
+                    [2912, 1, 1, 2, 1, 2, 2, 1, 1, "G"],
+                ],
+            )
+        )
+
+        mock_get_plots.side_effect = mock_plots
+
+        mock_plot_measurements = []
+        plot_measurements_columns = ["plot_id", "trait_id", "trait_qc", "trait_value"]
+
+        # for occurrence id 1 and trait id 1
+        mock_plot_measurements.append(
+            DataFrame(
+                columns=plot_measurements_columns,
+                data=[
+                    [2909, 1, "G", 6.155850575],
+                    [2910, 1, "G", 6.751358238],
+                ],
+            )
+        )
+
+        # for occurrence id 2 and trait id 1
+        mock_plot_measurements.append(DataFrame(columns=plot_measurements_columns, data=[]))
+        mock_get_plot_measurements.side_effect = mock_plot_measurements
+
+        mock_traits = []
+        test_trait = {"trait_id": 1, "trait_name": "trait_name_1", "abbreviation": "trait_abbrev_1"}
+        mock_traits.append(Trait(**test_trait))
+        mock_get_trait.side_effect = mock_traits
+
+        expected_columns = ""
+        for field in test_config["Analysis_Module"]["fields"]:
+            expected_columns += field["stat_factor"] + ","
+        expected_columns += "trait_abbrev_1"
+
+        expected_data_file_1_contents = (
+            "loc,expt,entry,plot,col,row,rep,trait_abbrev_1\n"
+            "1,1,1,2909,1,1,1,6.155850575\n"
+            "1,1,1,2910,1,2,1,6.751358238\n"
+        )
+
+        expected_data_file_2_contents = (
+            "loc,expt,entry,plot,col,row,rep,trait_abbrev_1\n" "1,1,1,2911,2,1,1,NA\n" "1,1,1,2912,2,2,1,NA\n"
+        )
+
+        expected_job_file_1 = get_job_file_template().format(
+            job_file_id="test_id_1",
+            trait_abbreviation="trait_abbrev_1",
+        )
+
+        expected_job_file_1 = get_job_file_template().format(
+            job_file_id="test_id_1_1",
+            trait_abbreviation="trait_abbrev_1",
+        )
+
+        expected_job_file_2 = get_job_file_template().format(
+            job_file_id="test_id_2_1",
+            trait_abbreviation="trait_abbrev_1",
+        )
+
+        output_folder = TemporaryDirectory()
+
+        results = ProcessData("EBS", "http://test.org", "test").run(test_request, test_config, output_folder.name)
+
+        self.assertEqual(len(results), 2)
+
+        self.assertTrue("asreml_job_file" in results[0])
+        with open(results[0]["asreml_job_file"]) as job_f_:
+            job_file_contents = job_f_.read()
+            self.assertEqual(job_file_contents, expected_job_file_1)
+
+        self.assertTrue("data_file" in results[0])
+        with open(results[0]["data_file"]) as data_f_:
+            data_file_contents = data_f_.read()
+            self.assertEqual(data_file_contents, expected_data_file_1_contents)
+
+        self.assertTrue("asreml_job_file" in results[1])
+        with open(results[1]["asreml_job_file"]) as job_f_:
+            job_file_contents = job_f_.read()
+            self.assertEqual(job_file_contents, expected_job_file_2)
+
+        self.assertTrue("data_file" in results[1])
+        with open(results[1]["data_file"]) as data_f_:
+            data_file_contents = data_f_.read()
+            self.assertEqual(data_file_contents, expected_data_file_2_contents)
