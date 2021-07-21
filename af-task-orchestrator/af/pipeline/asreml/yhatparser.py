309d0c5d5a8f5fe9a89ff2e221c7bbb1340dc319
Rodolfo N. Duldulao, Jr <rnduldulaojr@gmail.com>
2021-07-22 00:40:42 +0800

    Fix invalid escape sequence warning

--- af-task-orchestrator/af/pipeline/asreml/yhatparser.py
+++ af-task-orchestrator/af/pipeline/asreml/yhatparser.py
@@ -14,7 +14,7 @@ def _jsonize(*args, **kwargs):
 
 def parse(yhatfile) -> pd.DataFrame:
     try:
-        df = pd.read_csv(yhatfile, delimiter="\s+")
+        df = pd.read_csv(yhatfile, delimiter=r"\s+")
         df.rename(columns=YHAT_FILE_COLUMNS_TO_DB_COLUMNS, inplace=True)
         agg_cols = [x for x in df.columns if x not in set(static)]
         df = df.join(df[agg_cols].agg(_jsonize, axis=1).to_frame("additional_info")).drop(agg_cols, 1)
