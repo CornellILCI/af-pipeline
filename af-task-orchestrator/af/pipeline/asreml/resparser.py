d98ba00577f609d51a307e8960c66b342e6793ed
vince <vsp35@cornell.edu>
2021-07-23 10:02:42 -0400

    updated res parsing

--- af-task-orchestrator/af/pipeline/asreml/resparser.py
+++ af-task-orchestrator/af/pipeline/asreml/resparser.py
@@ -1,8 +1,10 @@
+from tempfile import NamedTemporaryFile
+
 import pandas as pd
 import re
 
 
-codes = [" SD", "STND"]
+sd = [" SD", "STND"]
 
 f67 = "/home/vince/dev/work/wfiles/resPedro/templatesB/67ac5d6f-5cdc-45fd-a2fa-2bc17a2c58bg_SA_0000/results/67ac5d6f-5cdc-45fd-a2fa-2bc17a2c58bg_SA_1001.res"
 f68 = "/home/vince/dev/work/wfiles/resPedro/templatesB/68ac5d6f-5cdc-45fd-a2fa-2bc17a2c58ab_SA_0000/results/68ac5d6f-5cdc-45fd-a2fa-2bc17a2c58ab_SA_1001.res"
@@ -11,13 +13,12 @@ f84 = "/home/vince/dev/work/wfiles/resPedro/templatesB/84ac5d6f-5cdc-45fd-a2fa-2
 f85 = "/home/vince/dev/work/wfiles/resPedro/templatesB/85/results/85.res"
 
 li = [f67, f68, f69, f84, f85]
-# df = pd.DataFrame(columns=['Record','SD'])
 
 def check_file(file) :
     try:
         with open (file, "rt") as f:
             file = f.readlines()
-            res = list(filter(lambda x: any(True for c in codes if c in x), file))
+            res = list(filter(lambda x: any(True for c in sd if c in x), file))
         return res
     except KeyError:
         raise ColumnNotAvailableError(file)
@@ -31,32 +32,40 @@ parse_res checks if there is SD and then extracts appropriate data
 def parse_res(res)-> pd.DataFrame:
     x = 0
     if len(res) > 0:
+
         df = pd.DataFrame(columns=['Record','SD'])
 
 
         for row in res :
 
 
-            if codes[0] in row:
+            if sd[0] in row:
 
                 SD = re.search('\[(.*?)\]', res[x]).group(1)
                 SD = [SD, row[-9:-4]]
                 df.loc[len(df.index)] = SD
                 x = x+1
 
-            if codes[1] in row:
+            if sd[1] in row:
 
                 SD = [row[12:16], row[-6:-1]]
                 df.loc[len(df.index)] = SD
 
-            # return df
         print(df)
         return df
 
 
     if len(res) == 0 :
-        print("No Standard Deviation\n")
+        print("no outliers found\n")
 
+# s67 and testdf are the same strings im using in the txt file
+s67 = '\t\t\t\t\t  Record     SD\n0  section 11, column 14 (of 15), row 22 (of 28)   3.70\n1   section 11, column 15 (of 15), row 2 (of 28)   3.61'
+print(s67)
 
-# parse_res(check_file(f69))
+testDf = pd.DataFrame({'Record': ['section 11, column 14 (of 15), row 22 (of 28)', 'section 11, column 15 (of 15), row 2 (of 28)'],
+                           'SD': [' 3.70', ' 3.61'] })
+print(testDf, "\n\n\n")
+
+# runs the parser
+# parse_res(check_file(f67))
 [parse_res(check_file(r)) for r in li]
--- af-task-orchestrator/af/tests/pipeline/test_res_parser.py
+++ af-task-orchestrator/af/tests/pipeline/test_res_parser.py
@@ -10,14 +10,12 @@ def test_res_parser():
     write a mock df string to a temp file
     """
     t = NamedTemporaryFile()
-    s67 = '\t\t\t\t\t  Record     SD\n0  section 11, column 14 (of 15), row 22 (of 28)   3.70'
+    s67 = '\t\t\t\t\t  Record     SD\n0  section 11, column 14 (of 15), row 22 (of 28)   3.70\n1   section 11, column 15 (of 15), row 2 (of 28)   3.61'
     t.write(bytes(s67, 'UTF-8'))
     t.seek(0)
 
-    testDf = pd.DataFrame(columns=['Record','SD'])
-
-    testDf.loc[0] = pd.Series({'Record': 'section 11, column 14 (of 15), row 22 (of 28)',
-                              'SD': '3.70' })
+    testDf = pd.DataFrame({'Record': ['section 11, column 14 (of 15), row 22 (of 28)', 'section 11, column 15 (of 15), row 2 (of 28)'],
+                               'SD': [' 3.70', ' 3.61'] })
 
 
     handler = parser.parse_res(parser.check_file(t.name))
