fe6ab804e4cccd0ee3923e55363d9c49b4ea630f
Rodolfo N. Duldulao, Jr <rnduldulaojr@gmail.com>
2021-07-24 00:38:46 +0800

    Removed .env from repo

--- af-task-orchestrator/start-worker.sh
+++ af-task-orchestrator/start-worker.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # Activate asreml license
+echo $ASREML_ACTIVATION_CODE
 asreml -z $ASREML_ACTIVATION_CODE
 
-celery multi start default ASREML -c:default 10 -c:ASREML ${ASREML_SEATS:-1} -A af.orchestrator.app worker  -l $LOG_LEVEL
+
+celery multi start default ASREML -c:default 10 -c:ASREML ${ASREML_SEATS:-1}  -Q:default default -Q:ASREML ASREML -A af.orchestrator.app worker  -l $LOG_LEVEL
