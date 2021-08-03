8eacc89aa20d9d7b003d73876eac93a55d1e29c2
Vishnu Govindaraj <vg249@cornell.edu>
2021-08-03 17:14:47 -0400

    Squashed 'db/' changes from 0e50143..ce0a88b
    ce0a88b Fix column name for analysis table
    
    git-subtree-dir: db
    git-subtree-split: ce0a88b715b58f8308af8de082c5b68e00184184

--- build/liquibase/changesets/21.07/schema/change_varchar_columns_to_text_columns.sql
+++ build/liquibase/changesets/21.07/schema/change_varchar_columns_to_text_columns.sql
@@ -4,29 +4,9 @@
 --comment: BA-578 Chnage columns of type VARCHAR to columns of type TEXT
 
 
--- af     | analysis          
--- af     | effect            
--- af     | fitted_values     
--- af     | job               
--- af     | job_data          
--- af     | job_stat_factor   
--- af     | model_stat        
--- af     | prediction        
--- af     | property          
--- af     | property_acl      
--- af     | property_config   
--- af     | property_meta     
--- af     | property_rule     
--- af     | property_ui       
--- af     | request           
--- af     | request_entry     
--- af     | request_parameter 
--- af     | task              
--- af     | variance          
-
 ALTER TABLE af.analysis
     ALTER COLUMN name TYPE text,
-    ALTER COLUMN deacription TYPE text,
+    ALTER COLUMN description TYPE text,
     ALTER COLUMN status TYPE text,
     ALTER COLUMN creator_id TYPE text,
     ALTER COLUMN modifier_id TYPE text;
