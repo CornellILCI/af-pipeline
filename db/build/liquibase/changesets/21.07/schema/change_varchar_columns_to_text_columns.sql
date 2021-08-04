e4baeef31eec46135b6363745e1140f6632fa229
Vishnu Govindaraj <vg249@cornell.edu>
2021-08-04 12:00:08 -0400

    Fix liquibase driver path
    Fix rabbitmq failure dure to deprecated variables
    
    Clean docker compose of unused volumes

--- db/build/liquibase/changesets/21.07/schema/change_varchar_columns_to_text_columns.sql
+++ db/build/liquibase/changesets/21.07/schema/change_varchar_columns_to_text_columns.sql
@@ -10,3 +10,34 @@ ALTER TABLE af.analysis
     ALTER COLUMN status TYPE text,
     ALTER COLUMN creator_id TYPE text,
     ALTER COLUMN modifier_id TYPE text;
+
+
+ALTER TABLE af.effect
+    ALTER COLUMN type TYPE text,
+    ALTER COLUMN creator_id TYPE text,
+    ALTER COLUMN modifier_id TYPE text;
+
+
+ALTER TABLE af.job
+    ALTER COLUMN name TYPE text,
+    ALTER COLUMN time_start TYPE text,
+    ALTER COLUMN time_end TYPE text,
+    ALTER COLUMN output_path TYPE text,
+    ALTER COLUMN status TYPE text,
+    ALTER COLUMN status_message TYPE text,
+    ALTER COLUMN size TYPE text,
+    ALTER COLUMN creator_id TYPE text,
+    ALTER COLUMN modifier_id TYPE text;
+
+
+ALTER TABLE af.job_data
+    ALTER COLUMN data TYPE text,
+    ALTER COLUMN creator_id TYPE text,
+    ALTER COLUMN modifier_id TYPE text;
+
+
+ALTER TABLE af.job_stat_factor
+    ALTER COLUMN creator_id TYPE text,
+    ALTER COLUMN modifier_id TYPE text;
+
+
