d1cbf802894dfbea49f3af3ab76deb2738584d53
Rodolfo N. Duldulao, Jr <rnduldulaojr@gmail.com>
2021-08-06 00:17:51 +0800

    Added unit tests for asr not converged results

--- af-task-orchestrator/af/tests/pipeline/test_asreml_service.py
+++ af-task-orchestrator/af/tests/pipeline/test_asreml_service.py
@@ -18,6 +18,19 @@ def test_simple_test_1(dbsession, sample_asreml_result_string_1):
     assert dbsession.query(Prediction.id).count() == 180
 
 
+def test_asr_not_converged_result(dbsession, sample_asreml_not_converged_result_string):
+    # create test stream from sample_asreml_result_string_1
+    sample_stream = io.StringIO(sample_asreml_not_converged_result_string)
+    sample_job_id = 123
+
+    process_asreml_result(dbsession, sample_job_id, sample_stream)
+
+    # check objects saved
+    assert dbsession.query(Variance.id).count() == 0  # there should be no variance objects saved
+    assert dbsession.query(ModelStat.id).count() == 1  # model stat should be saved
+    assert dbsession.query(Prediction.id).count() == 0  # there should be no prediction objects saved.
+
+
 def test_yhat_parser_service_happy_path(sample_yhat_data_1, dbsession):
 
     process_yhat_result(dbsession, 1, sample_yhat_data_1)
