733d09d5a087f4f3c15371b373f857e792efeb60
Vishnu Govindaraj <vg249@cornell.edu>
2021-10-06 12:42:40 -0400

    Fix db model update conflict caused by BA-656

--- af-task-orchestrator/af/pipeline/asreml/services.py
+++ af-task-orchestrator/af/pipeline/asreml/services.py
@@ -44,10 +44,11 @@ def process_asreml_result(session, job_id: int, filename_or_stream, *args, **kwa
         model_stat.creation_timestamp = datetime.utcnow()
 
         session.add(model_stat)
-
-        if model_stat.is_converged:
-            _save_variances(session, ch.variances)
-            _save_predictions(session, ch.predictions)
+        
+        # TODO: Tables need to be restructured
+        #if model_stat.is_converged:
+        #    _save_variances(session, ch.variances)
+        #    _save_predictions(session, ch.predictions)
 
     session.commit()
 
