c8e6c12539bdd187d446e63bac86aac52568a9e8
Vishnu Govindaraj <vg249@cornell.edu>
2021-10-28 13:04:25 -0400

    Add convert api_models to json response method common utis

---
+++ af-jobs-api/common/responses.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from http import HTTPStatus
+
+from flask import make_response
+
+def json_response(response_model: BaseModel, http_status: HTTPStatus):
+    
+    response = make_response(response_model.json(exclude_none=True), http_status)
+    response.mimetype = 'application/json'
+
+    return response
+
+
