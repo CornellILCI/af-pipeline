530727ac7a9a964e5b30dcdc7ace787f0dcbcd59
Rodolfo N. Duldulao, Jr <rnduldulaojr@gmail.com>
2022-02-10 00:15:34 +0800

    Config 0007 and config 0008 liquibase update scripts

---
+++ db/build/liquibase/changesets/22.02/data/template/add_formulas_for_config_00007_00008.sql
@@ -0,0 +1,58 @@
+-- add formula to config_00007
+WITH config_formula AS (
+	INSERT INTO af.property(
+		type, data_type, code, "name", "label", "statement"
+	) VALUES (
+		'catalog_item', 'character varying', 'formula_opt7', 
+		'RCBD SEML Univariate - G x E as CORUH', 
+		'RCBD SEML Univariate - G x E as CORUH', 
+		'{trait_name} ~ mu loc !r coruh(loc).id(entry) idh(loc).rep !f mv'
+	) RETURNING id
+),
+config_formula_property_config_link AS (
+	INSERT INTO af.property_config(
+		order_number, creation_timestamp, creator_id,
+		is_void, property_id, config_property_id, is_layout_variable
+	) VALUES (
+		1, 'now()', '1', false,
+		(SELECT id FROM af.property WHERE code='formula'), 
+		(SELECT id FROM config_formula), false
+	)
+),
+config_formula_config_00007_link AS (
+	UPDATE af.property_config
+    SET config_property_id = (SELECT id FROM config_formula)
+    WHERE property_id = (SELECT id FROM af.property WHERE code = 'config_00007.cfg')
+    AND config_property_id=149
+)
+SELECT * FROM config_formula;
+
+
+-- add formula to config_00008
+WITH config_formula AS (
+	INSERT INTO af.property(
+		type, data_type, code, "name", "label", "statement"
+	) VALUES (
+		'catalog_item', 'character varying', 'formula_opt8', 
+		'Alpha-Lattice MET Univariate - G x E as CORUH', 
+		'Alpha-Lattice MET Univariate - G x E as CORUH', 
+		'{trait_name} ~ mu loc loc.rep !r idh(loc).rep.block coruh(loc).id(entry) !f mv'
+	) RETURNING id
+),
+config_formula_property_config_link AS (
+	INSERT INTO af.property_config(
+		order_number, creation_timestamp, creator_id,
+		is_void, property_id, config_property_id, is_layout_variable
+	) VALUES (
+		1, 'now()', '1', false,
+		(SELECT id FROM af.property WHERE code='formula'), 
+		(SELECT id FROM config_formula), false
+	)
+),
+config_formula_config_00008_link AS (
+	UPDATE af.property_config
+    SET config_property_id = (SELECT id FROM config_formula)
+    WHERE property_id = (SELECT id FROM af.property WHERE code = 'config_00008.cfg')
+    AND config_property_id=151
+)
+SELECT * FROM config_formula;
