267b3cbd75e2676091a0a309860581aa31abda7d
Vishnu Govindaraj <vg249@cornell.edu>
2022-03-21 13:24:54 -0400

    Add Asreml-R DPO

---
+++ af-task-orchestrator/af/pipeline/asreml_r/dpo.py
@@ -0,0 +1,14 @@
+from af.pipeline.job_data import JobData
+from af.pipeline.dpo import ProcessData
+
+class AsremlRProcessData(ProcessData):
+
+    def __init__(self):
+        pass
+
+    def mesl(self):
+        return JobData()
+
+    def run(self):
+        pass
+
--- af-task-orchestrator/af/tests/pipeline/test_asreml_r_dpo.py
+++ af-task-orchestrator/af/tests/pipeline/test_asreml_r_dpo.py
@@ -1,4 +1,9 @@
+from af.pipeline.job_data import JobData
+
+from af.pipeline.asreml_r import dpo
+
+def test_mesl_returns_job_data():
+    asreml_r_dpo = dpo.AsremlRProcessData()
+    assert type(asreml_r_dpo.mesl()) is JobData
 
 
-def test_sesl():
-    pass
