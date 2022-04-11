8e421c72d14836fa99f161e0c172b94d33f9906c
Vishnu Govindaraj <vg249@cornell.edu>
2022-04-10 20:04:57 -0400

    Add H2 calculation to analysis report
    Check zero division error

--- af-task-orchestrator/af/tests/pipeline/test_calculation_engine.py
+++ af-task-orchestrator/af/tests/pipeline/test_calculation_engine.py
@@ -1,5 +1,5 @@
 from af.pipeline import calculation_engine
-
+import pytest
 
 def test_get_h2_cullis():
     assert calculation_engine.get_h2_cullis(2, 2) is not None
@@ -13,3 +13,10 @@ def test_get_h2_cullis_return_correct_output():
     average_standard_error = 2
 
     assert calculation_engine.get_h2_cullis(variance, average_standard_error) == expected_output
+
+def test_zero_division():
+
+    with pytest.raises(ZeroDivisionError):
+        genetic_variance = 0
+        average_standard_error = 10
+        calculation_engine.get_h2_cullis(genetic_variance, average_standard_error)
