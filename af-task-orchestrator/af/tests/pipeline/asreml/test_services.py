15b6ac39fa241037ce6f8b1db8847068b68da279
Vishnu Govindaraj <vg249@cornell.edu>
2022-04-14 16:34:57 -0400

    Retract some test cases

--- af-task-orchestrator/af/tests/pipeline/asreml/test_services.py
+++
@@ -1,6 +0,0 @@
-from af.pipeline import asreml
-
-
-def test_process_result_returns_asreml_result(dbsession):
-
-    assert type(asreml.services.process_asreml_result(dbsession, 1, "")) == type(asreml.services.AsremlResult)
