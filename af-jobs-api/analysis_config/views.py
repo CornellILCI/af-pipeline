e803c7962c50e42785f6881588b9e99b72d9d300
sb2597 <62260853+sb2597@users.noreply.github.com>
2022-04-28 16:09:34 -0400

    created new endpoint and service

--- af-jobs-api/analysis_config/views.py
+++ af-jobs-api/analysis_config/views.py
@@ -38,27 +38,42 @@ def list():
     return json_response(response, HTTPStatus.OK)
 
 
+# @analysis_models_bp.route("", methods=["POST"])
+# @validate_api_request(body_model=api_models.AnalysisRequestParameters)
+# def post_analysis_config():
+#     """Create request object based on body params"""
+#     request_query_params = api_models.AnalysisConfigsListQueryParameters(**request.args)
+
+#     filter_params = request_query_params.as_db_filter_params()
+
+#     analysis_configs, total_count = service.get_analysis_configs(
+#         page=request_query_params.page, page_size=request_query_params.pageSize, **filter_params
+#     )
+
+#     # DTOs for api response
+#     analysis_config_dtos = []
+
+#     for analysis_config in analysis_configs:
+#         analysis_config_dtos.append(api_models.map_property(analysis_config))
+
+#     response = api_models.AnalysisConfigListResponse(
+#         metadata=api_models.create_metadata(request_query_params.page, request_query_params.pageSize, total_count),
+#         result=api_models.AnalysisConfigListResponseResult(data=analysis_config_dtos),
+#     )
+
+#     return json_response(response, HTTPStatus.OK)
+
 @analysis_models_bp.route("", methods=["POST"])
-@validate_api_request(body_model=api_models.AnalysisRequestParameters)
 def post_analysis_config():
-    """Create request object based on body params"""
-    request_query_params = api_models.AnalysisConfigsListQueryParameters(**request.args)
 
-    filter_params = request_query_params.as_db_filter_params()
 
-    analysis_configs, total_count = service.get_analysis_configs(
-        page=request_query_params.page, page_size=request_query_params.pageSize, **filter_params
+
+    service.create_analysis_config(
+    property_code, property_configName, property_label, property_description, property_design, property_data_type, property_creator_id, property_modifier_id, property_tenant_id, property_id, property_statement,
+    property_meta_version, property_meta_date, property_meta_author, property_meta_email, property_meta_organization_code, property_meta_engine, property_meta_breding_program_id,
+    property_meta_pipeline_id, property_meta_stage_id, property_meta_design, property_meta_trait_level, property_meta_analysis_objective, property_meta_exp_analysis_pattern,
+    property_meta_loc_analysis_pattern, property_meta_year_analysis_pattern, property_meta_trait_pattern
     )
 
-    # DTOs for api response
-    analysis_config_dtos = []
-
-    for analysis_config in analysis_configs:
-        analysis_config_dtos.append(api_models.map_property(analysis_config))
-
-    response = api_models.AnalysisConfigListResponse(
-        metadata=api_models.create_metadata(request_query_params.page, request_query_params.pageSize, total_count),
-        result=api_models.AnalysisConfigListResponseResult(data=analysis_config_dtos),
-    )
-
+    response = {}
     return json_response(response, HTTPStatus.OK)
